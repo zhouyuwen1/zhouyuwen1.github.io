c1578b282188cc2db2c9a00e334872b9b62bf0bd
zhouyuwen1 <117556805+zhouyuwen1@users.noreply.github.com>
2024-11-18 18:07:43 +0800

    Update birthday.js

--- docs/zyw-tech/Birthday/006-cjy/birthday.js
+++ docs/zyw-tech/Birthday/006-cjy/birthday.js
@@ -612,7 +612,7 @@ S.Dot = function (x, y) {
   this.e = 0.07;
   this.s = true;
 
-  this.c = new S.Color(250, 128, 114, this.p.a);
+  this.c = new S.Color(0, 250, 154, this.p.a);
 
   this.t = this.clone();
   this.q = [];
